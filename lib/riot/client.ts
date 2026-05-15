cd0369a6ffa6522b647e9b2f66c023f9193f066a
AlexMcKwi <alexander.makkaoui@gmail.com>
2026-05-15 13:01:07 +0200

    trive riot api data into app correctly

--- lib/riot/client.ts
+++ lib/riot/client.ts
@@ -1,5 +1,6 @@
-const RIOT_API = "https://europe.api.riotgames.com"
-const RIOT_ACCOUNT_API = "https://americas.api.riotgames.com"
+const RIOT_API = "https://europe.api.riotgames.com/"
+const RIOT_ACCOUNT_API = "https://americas.api.riotgames.com/"
+const SUMMONER_API = "https://euw1.api.riotgames.com/"
 
 export async function riotFetch(path: string, region: string = RIOT_API) {
   const response = await fetch(`${region}${path}`, {
@@ -18,11 +19,11 @@ export async function riotFetch(path: string, region: string = RIOT_API) {
 
 export async function getAccountByGameName(gameName: string, tagLine: string) {
   return riotFetch(
-    `/riot/account/v1/accounts/by-game-and-tag/${encodeURIComponent(gameName)}/${encodeURIComponent(tagLine)}`,
-    RIOT_ACCOUNT_API
+    `riot/account/v1/accounts/by-riot-id/${encodeURIComponent(gameName)}/${encodeURIComponent(tagLine)}`,
+    RIOT_API
   )
 }
 
 export async function getSummonerByPuuid(puuid: string) {
-  return riotFetch(`/lol/summoner/v4/summoners/by-puuid/${puuid}`)
+  return riotFetch(`lol/summoner/v4/summoners/by-puuid/${puuid}`,SUMMONER_API)
 }
--- lib/riot/matches.ts
+++ lib/riot/matches.ts
@@ -1,11 +1,14 @@
 import { riotFetch } from "./client"
 
+const RIOT_API = "https://europe.api.riotgames.com/"
+
 export async function getMatchIds(puuid: string) {
   return riotFetch(
-    `/lol/match/v5/matches/by-puuid/${puuid}/ids?start=0&count=20`
+    `lol/match/v5/matches/by-puuid/${puuid}/ids?start=0&count=20`,
+    RIOT_API
   )
 }
 
 export async function getMatch(matchId: string) {
-  return riotFetch(`/lol/match/v5/matches/${matchId}`)
+  return riotFetch(`lol/match/v5/matches/${matchId}`, RIOT_API)
 }
